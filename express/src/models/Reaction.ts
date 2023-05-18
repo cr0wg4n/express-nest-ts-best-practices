9494b901521995050fb1f63dfdd86f60f7bc3474
Imer <imertpro@gmail.com>
2023-05-18 17:30:19 -0400

    demo commit 2

--- express/src/models/Reaction.ts
+++ express/src/models/Reaction.ts
@@ -1,12 +1,28 @@
 import { Comment } from './Comment';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
+
+export enum ReactionType {
+  LIKE='LIKE',
+  LOVE='LOVE',
+  CARE='CARE',
+  HAHA='HAHA',
+  WOW='WOW',
+  SAD='SAD',
+  ANGRY='ANGRY',
+  NOTHING='NOTHING'
+}
+
+
 @Entity()
 export class Reaction {
   @PrimaryGeneratedColumn()
   id: string
 
-  @Column()
+  @Column({
+    type: 'enum',
+    enum: ReactionType,
+  })
   type: string
 
   @ManyToOne(() => Comment, comment => comment.reactions)
